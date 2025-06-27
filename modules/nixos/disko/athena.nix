684d9b1ad5e276c3f2e194900f4c7894a77ce2fa
João Pedro Castro <jpcastro.sp@gmail.com>
2025-06-27 12:29:31 +0000

    Swap partition in athena

--- modules/nixos/disko/athena.nix
+++ modules/nixos/disko/athena.nix
@@ -15,11 +15,12 @@
             boot = {
               name = "boot";
               size = "1M";
-              type = "EF02";
+              type = "EF02"; # BIOS boot partition
             };
             ESP = {
+              name = "ESP";
               size = "512M";
-              type = "EF00";
+              type = "EF00"; # EFI System Partition
               content = {
                 type = "filesystem";
                 format = "vfat";
@@ -28,47 +29,90 @@
               };
             };
             luks = {
+              name = "luks";
               size = "100%";
-              start = "250G";
               content = {
                 type = "luks";
                 name = "crypted";
-
                 settings = {
                   allowDiscards = true;
                   keyFile = "/tmp/secret.key";
                 };
-                # additionalKeyFiles = ["/tmp/additionalSecret.key"];
                 content = {
-                  type = "btrfs";
-                  extraArgs = ["-f"];
-                  subvolumes = {
-                    "/root" = {
-                      mountpoint = "/";
-                      mountOptions = [
-                        "noatime"
-                        "compress=zstd"
-                      ];
-                    };
-                    "/home" = {
-                      mountpoint = "/home";
-                      mountOptions = [
-                        "noatime"
-                        "compress=zstd"
-                      ];
-                    };
-                    "/nix" = {
-                      mountpoint = "/nix";
-                      mountOptions = [
-                        "noatime"
-                        "compress=zstd"
-                      ];
-                    };
-                    "/persist" = {
-                      mountpoint = "/persist";
-                      mountOptions = ["noatime" "compress=zstd"];
-                    };
-                  };
+                  type = "lvm_pv";
+                  vg = "pool";
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+
+    lvm_vg = {
+      pool = {
+        type = "lvm_vg";
+        lvs = {
+          swap = {
+            size = swapSizeInGb;
+            content = {
+              type = "swap";
+              resumeDevice = true; # Enable hibernation
+            };
+          };
+          root = {
+            size = "100%FREE";
+            content = {
+              type = "btrfs";
+              extraArgs = ["-f"];
+              subvolumes = {
+                "/root" = {
+                  mountpoint = "/";
+                  mountOptions = [
+                    "noatime"
+                    "compress=zstd:1"
+                    "space_cache=v2"
+                  ];
+                };
+                "/home" = {
+                  mountpoint = "/home";
+                  mountOptions = [
+                    "noatime"
+                    "compress=zstd:1"
+                    "space_cache=v2"
+                  ];
+                };
+                "/nix" = {
+                  mountpoint = "/nix";
+                  mountOptions = [
+                    "noatime"
+                    "compress=zstd:1"
+                    "space_cache=v2"
+                  ];
+                };
+                "/persist" = {
+                  mountpoint = "/persist";
+                  mountOptions = [
+                    "noatime"
+                    "compress=zstd:1"
+                    "space_cache=v2"
+                  ];
+                };
+                "/var/log" = {
+                  mountpoint = "/var/log";
+                  mountOptions = [
+                    "noatime"
+                    "compress=zstd:1"
+                    "space_cache=v2"
+                  ];
+                };
+                "/.snapshots" = {
+                  mountpoint = "/.snapshots";
+                  mountOptions = [
+                    "noatime"
+                    "compress=zstd:1"
+                    "space_cache=v2"
+                  ];
                 };
               };
             };
