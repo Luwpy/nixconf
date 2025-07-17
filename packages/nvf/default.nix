6bf95f1dafbb98ef52c16d7da506612990e22cbd
luwpy <jpcastro.sp@gmail.com>
2025-07-17 12:43:36 +0000

    finished nvf

--- packages/nvf/default.nix
+++ packages/nvf/default.nix
@@ -1,8 +1,24 @@
-{ ...
-}: let lib = import ./../../modules/flake/lib; 
-      core = lib.importModulesRecursive ./core;
-      theme = lib.importModulesRecursive ./theme;
-      plugins = lib.importModulesRecursive ./plugins;
+{lib, ...}: let
+  # Simple recursive module importer
+  importModulesRecursive = dir: let
+    entries = builtins.readDir dir;
+
+    processEntry = name: type: let
+      path = "${toString dir}/${name}";
+    in
+      if type == "directory" && builtins.pathExists "${path}/default.nix"
+      then import path
+      else if type == "regular" && name != "default.nix" && lib.hasSuffix ".nix" name
+      then import path
+      else null;
+
+    modules = lib.mapAttrsToList processEntry entries;
+  in
+    lib.filter (x: x != null) modules;
+
+  core = importModulesRecursive ./core;
+  theme = importModulesRecursive ./theme;
+  plugins = importModulesRecursive ./plugins;
 in {
-  imports =  core ++ theme ++ plugins;
+  imports = core ++ theme ++ plugins;
 }
